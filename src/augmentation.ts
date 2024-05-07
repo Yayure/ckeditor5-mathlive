e00661c2e8398d65470cf61afd02929ecd7074a0
Yayure <satella000@qq.com>
2024-05-07 11:21:06 +0800

    feat: Update configuration parameter comments

--- src/augmentation.ts
+++ src/augmentation.ts
@@ -11,20 +11,29 @@ declare module '@ckeditor/ckeditor5-core' {
 	interface EditorConfig {
 		mathlive?: {
 
-			/**  Customize your formula panel. */
+			/**
+			 * Customize your formula panel.
+			 */
 			renderMathPanel?: ( element: HTMLElement ) => ( () => void ) | undefined;
 
-			/**  A string used as a regular expression of class names of elements whose content will be scanned for delimiters.
+			/**
+			 * A string used as a regular expression of class names of elements whose content will be scanned for delimiters.
 			 * e.g. <span class="tex2jax_process">\( \sqrt{\frac{a}{b}} \)</span>
-			 * */
+			 */
 			processClass?: string;
 
-			/**  <script> tags with this type will be processed as LaTeX.
+			/**
+			 * <script> tags with this type will be processed as LaTeX.
 			 * e.g. <script type="math/tex">\( \sqrt{\frac{a}{b}} \)</script>
-			 * */
+			 */
 			processScriptType?: string;
 
-			/**  Output the className of html data (e.g. <span class="ck-mathlive-tex">\( \sqrt{\frac{a}{b}} \)</span>) */
+			/**
+			 * Output the element of html data.
+			 * e.g.
+			 * { type: 'script', attributes: { type: 'math/tex' } } => <script type="math/tex">\( \sqrt{\frac{a}{b}} \)</script>
+			 * { type: 'span', attributes: { class: 'tex2jax_process' } } => <span class="tex2jax_process">\( \sqrt{\frac{a}{b}} \)</span>
+			 */
 			output?: {
 				type: string;
 				attributes?: ElementAttributes;
